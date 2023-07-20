b548d8085d411f4e1f7db7e6c8cfd5adc278df28
BrunoSouza <desouza88@hotmail.com>
2023-07-20 16:30:36 -0300

    test: req02 - tests to test_dish.py implemented

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,51 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+import pytest
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient, Restriction
 
 
-# Req 2
 def test_dish():
-    pass
+    dish = Dish("Spaghetti Carbonara", 15.99)
+    assert dish.name == "Spaghetti Carbonara"
+
+    dish1 = Dish("Spaghetti Carbonara", 15.99)
+    dish2 = Dish("Spaghetti Carbonara", 15.99)
+    assert hash(dish1) == hash(dish2)
+
+    dish1 = Dish("Spaghetti Carbonara", 15.99)
+    dish2 = Dish("Pasta Bolognese", 14.99)
+    assert hash(dish1) != hash(dish2)
+
+    dish1 = Dish("Spaghetti Carbonara", 15.99)
+    dish2 = Dish("Spaghetti Carbonara", 15.99)
+    assert dish1 == dish2
+
+    dish1 = Dish("Spaghetti Carbonara", 15.99)
+    dish2 = Dish("Pasta Bolognese", 14.99)
+    assert dish1 != dish2
+
+    dish = Dish("Spaghetti Carbonara", 15.99)
+    assert repr(dish) == "Dish('Spaghetti Carbonara', R$15.99)"
+
+    with pytest.raises(TypeError):
+        Dish("Spaghetti Carbonara", "15.99")
+
+    with pytest.raises(ValueError):
+        Dish("Spaghetti Carbonara", -10)
+
+    dish = Dish("Spaghetti Carbonara", 15.99)
+    ingredient = Ingredient("manteiga")
+    dish.add_ingredient_dependency(ingredient, -2)
+    assert dish.recipe.get(ingredient) == -2
+
+    ingredient1 = Ingredient("manteiga")
+    ingredient2 = Ingredient("queijo mussarela")
+    dish.add_ingredient_dependency(ingredient1, 2)
+    dish.add_ingredient_dependency(ingredient2, 1)
+
+    restrictions = dish.get_restrictions()
+    expected_restrictions = {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
+    assert restrictions == expected_restrictions
+
+    ingredients = dish.get_ingredients()
+    assert ingredient1 in ingredients
+    assert ingredient2 in ingredients
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -2,35 +2,28 @@ from src.models.ingredient import Ingredient, Restriction
 
 
 def test_ingredient():
-    # Req 1.1
     ingredient1 = Ingredient("manteiga")
     ingredient2 = Ingredient("manteiga")
     assert hash(ingredient1) == hash(ingredient2)
 
-    # Req 1.2
     ingredient1 = Ingredient("manteiga")
     ingredient2 = Ingredient("farinha")
     assert hash(ingredient1) != hash(ingredient2)
 
-    # Req 1.3
     ingredient1 = Ingredient("queijo mussarela")
     ingredient2 = Ingredient("queijo mussarela")
     assert ingredient1 == ingredient2
 
-    # Req 1.4
     ingredient1 = Ingredient("queijo mussarela")
     ingredient2 = Ingredient("farinha")
     assert ingredient1 != ingredient2
 
-    # Req 1.5
     ingredient = Ingredient("manteiga")
     assert repr(ingredient) == "Ingredient('manteiga')"
 
-    # Req 1.6
     ingredient = Ingredient("manteiga")
     assert ingredient.name == "manteiga"
 
-    # Req 1.7
     ingredient = Ingredient("queijo mussarela")
     expected_restrictions = {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
     assert ingredient.restrictions == expected_restrictions
