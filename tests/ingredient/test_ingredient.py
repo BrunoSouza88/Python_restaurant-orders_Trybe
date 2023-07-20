72a91c38cbbee22422c89424714b015ae46c6493
BrunoSouza <desouza88@hotmail.com>
2023-07-20 15:54:38 -0300

    test: req01 - tests to ingredient.py implemented

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,39 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
-# Req 1
 def test_ingredient():
-    pass
+    # Req 1.1
+    ingredient1 = Ingredient("manteiga")
+    ingredient2 = Ingredient("manteiga")
+    assert hash(ingredient1) == hash(ingredient2)
+
+    # Req 1.2
+    ingredient1 = Ingredient("manteiga")
+    ingredient2 = Ingredient("farinha")
+    assert hash(ingredient1) != hash(ingredient2)
+
+    # Req 1.3
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    assert ingredient1 == ingredient2
+
+    # Req 1.4
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("farinha")
+    assert ingredient1 != ingredient2
+
+    # Req 1.5
+    ingredient = Ingredient("manteiga")
+    assert repr(ingredient) == "Ingredient('manteiga')"
+
+    # Req 1.6
+    ingredient = Ingredient("manteiga")
+    assert ingredient.name == "manteiga"
+
+    # Req 1.7
+    ingredient = Ingredient("queijo mussarela")
+    expected_restrictions = {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
+    assert ingredient.restrictions == expected_restrictions
+
+
+test_ingredient()
